649b400ff23adde6610723afc180aec01da4b937
Bruno <brunogrzegork@gmail.com>
2022-02-26 17:06:15 -0300

    tabelas

--- src/main/java/br/edu/utfpr/commerce/App.java
+++ src/main/java/br/edu/utfpr/commerce/App.java
@@ -4,13 +4,42 @@ import br.edu.utfpr.commerce.model.local.Endereco;
 import br.edu.utfpr.commerce.model.local.Pais;
 import br.edu.utfpr.commerce.model.local.Estado;
 import br.edu.utfpr.commerce.model.local.Cidade;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
 
 public class App {
 
     public static void main(String[] args) {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("app-comercio");
+        
+        EntityManager em = factory.createEntityManager();
+        
         Pais br = new Pais("Brasil", "BR");
         Estado pr = new Estado("Paraná", "PR", br);
         Cidade medianeira = new Cidade("Medianeira", pr);
+        
+        EntityTransaction transaction = em.getTransaction();
+        
+        try{
+             transaction.begin(); //Iniciando a transação
+             
+             
+             //em.persist(br); //Persistindo o País Brasil
+             //em.persist(pr); //Persistindo o Estado do Paraná
+             em.persist(medianeira); //Persistindo a cidade de Medianeira 
+             
+             
+              transaction.commit(); //Efetivando as alterações do DB
+        }catch(Exception e){    
+            System.out.println("Falha ao executar transação:  "  +  e.getMessage());
+        transaction.rollback();        
+        } finally{
+        // em.close();
+         //factory.close();;
+        }
+        
         Endereco e = new Endereco("Rua A", "1234", "Cidade Alta", medianeira, "85884-000");
 
        // Pessoa juca = new PessoaFisica("Juca", e, "00011122233344", "12345");
--- src/main/java/br/edu/utfpr/commerce/model/local/Estado.java
+++ src/main/java/br/edu/utfpr/commerce/model/local/Estado.java
@@ -1,11 +1,33 @@
 package br.edu.utfpr.commerce.model.local;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_estado")
 public class Estado {
 
     // Atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+    
+    @Column(name = "nome", length = 150, nullable = false)
     private String nome;
+    
+    @Column(name = "sigla", length = 3, nullable = false)
     private String sigla;
+    
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "pais_id")
     private Pais pais;
 
     // Construtores
